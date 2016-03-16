4d615c52dda4ed6de6fcb5226487b35fe2d154bc
mmouterde <martin.mouterde@gmail.com>
2016-03-16 21:24:33 +0100

    nodify website

---
+++ server/server.js
@@ -0,0 +1,17 @@
+(function () {
+    'use strict';
+
+    var restify = require('restify');
+
+    var server = restify.createServer();
+
+    server.get(/\/*/, restify.serveStatic({
+        directory: './public',
+        default: 'index.html'
+    }));
+
+    server.listen(8080, function () {
+        console.log('Server running on 8080');
+    });
+
+})();
