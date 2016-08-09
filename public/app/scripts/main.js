0eb166076ec4f21cdb3639ab572f18857ef727ca
Cocoual Corentin <corentin.cocoual@gmail.com>
2016-08-09 16:56:29 +0200

    Removing callback in smoothCroll init

--- public/app/scripts/main.js
+++ public/app/scripts/main.js
@@ -1,14 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
     smoothScroll.init({
-        offset: "50px",
-        callback: function (anchor, toggle) {
-            disableNavItems();
-            toggle.classList.add('is-active');
-            if (hamburgerMenu.classList.contains('is-active')) {
-                hamburgerMenu.classList.remove('is-active');
-                nav.classList.remove('is-active');
-            }
-        }
+        offset: "50px"
     });
-
 });
