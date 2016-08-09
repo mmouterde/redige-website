43aa79ba12659e4ce85c959a1a5769ba15ef510c
Cocoual Corentin <corentin.cocoual@gmail.com>
2016-08-09 16:34:53 +0200

    Adding arrow to indicate to scroll down

--- public/app/scripts/main.js
+++ public/app/scripts/main.js
@@ -1,3 +1,14 @@
 document.addEventListener("DOMContentLoaded", function () {
+    smoothScroll.init({
+        offset: "50px",
+        callback: function (anchor, toggle) {
+            disableNavItems();
+            toggle.classList.add('is-active');
+            if (hamburgerMenu.classList.contains('is-active')) {
+                hamburgerMenu.classList.remove('is-active');
+                nav.classList.remove('is-active');
+            }
+        }
+    });
 
 });
