406e1453a91fd70a6e18d3703ff1c01c24ce49fc
mmouterde <martin.mouterde-ext@energiency.com>
2017-10-02 23:08:51 +0200

    update speech

--- public/app/scripts/main.js
+++ public/app/scripts/main.js
@@ -1,6 +1,6 @@
 NodeList.prototype.forEach = Array.prototype.forEach;
 
-var version = "0.2.5";
+var version = "0.2.6";
 var winURL = "http://download.redige.net/files/Redige Setup " + version + ".exe";
 var linURL = "http://download.redige.net/files/redigeApp-" + version + "-x86_64.AppImage";
 var macURL = "http://download.redige.net/files/Redige-" + version + ".dmg";
